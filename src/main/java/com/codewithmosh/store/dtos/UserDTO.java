e3aed580d530ef8c195c6ada6c0d3276a8a84501
nikita-sarkar <nikita2019sarkar@gmail.com>
2025-05-04 10:34:36 +0530

    Using JSONFormat to customize response data

--- src/main/java/com/codewithmosh/store/dtos/UserDTO.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDTO.java
@@ -1,12 +1,29 @@
 package com.codewithmosh.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @AllArgsConstructor
 @Getter
 public class UserDTO {
+    @JsonProperty("user_id")
     private Long id;
+
     private String name;
+
+    @JsonIgnore
     private String email;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String phoneNo;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 }
--- src/main/java/com/codewithmosh/store/mappers/UserMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -3,8 +3,10 @@ package com.codewithmosh.store.mappers;
 import com.codewithmosh.store.dtos.UserDTO;
 import com.codewithmosh.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDTO toDTO(User user);
 }
